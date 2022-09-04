59db2981a6a8812ea7ea74d8fb5d051cd692da4a
upiter-2005 <kuchenyov-pavel@mail.ru>
2022-09-04 11:02:50 +0300

    add color and arrows for prices

--- src/components/AskBid/index.jsx
+++ src/components/AskBid/index.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
 import styles from "./AskBid.module.scss";
 
 export default function AskBid() {
+  const colorPraice = useRef("");
+  const arrow = useRef("");
+
   const currency = useSelector((state) => state.activePair.currency);
   const exchangeTo = useSelector((state) => state.activePair.exchangeTo);
-  console.log(currency);
-  console.log(exchangeTo);
+
   const [ask, setAsk] = useState([]);
   const [bidList, setBidList] = useState([]);
   const [currentPrice, setCurrentPrice] = useState(0);
@@ -25,8 +27,18 @@ export default function AskBid() {
     const ws = new WebSocket(`wss://stream.binance.com:9443/ws/${pair}@trade`);
     ws.addEventListener("message", (e) => {
       const res = JSON.parse(e.data);
-      //   const roundPrice = parseInt(res.p).toFixed(2)
-      setCurrentPrice(parseFloat(res.p).toFixed(2));
+      const roundPrice = parseFloat(res.p).toFixed(2);
+      setCurrentPrice((prevCurrentPrice) => {
+        if (prevCurrentPrice > roundPrice) {
+          colorPraice.current = "low";
+          arrow.current = "arrow_downward";
+        } else {
+          colorPraice.current = "rise";
+          arrow.current = "arrow_upward";
+        }
+
+        return roundPrice;
+      });
     });
   }, []);
 
@@ -40,15 +52,24 @@ export default function AskBid() {
           </div>
         </div>
         {ask.map((obj, i) => (
-          <li key={i} className={styles.askBidItem}>
+          <li
+            key={i}
+            className={`${styles.askBidItem} ${styles.askBidInnerRed}`}
+          >
             <span>{obj[0]}</span>
             <br />
             <span>{obj[1]}</span>
           </li>
         ))}
       </div>
-      <div className={styles.askBidCurrentPrice}>{currentPrice}</div>
-      <div className={styles.askBidInner}>
+      {/* <div className={styles.askBidCurrentPrice}>{currentPrice}</div> */}
+      <div className={`${colorPraice.current} currentPrice`}>
+        {currentPrice}
+        <span className="material-symbols-outlined askBidArrow">
+          {arrow.current}
+        </span>
+      </div>
+      <div className={`${styles.askBidInner} `}>
         <div className={styles.askBidTitle}>
           <div>Price</div>
           <div>
@@ -56,7 +77,10 @@ export default function AskBid() {
           </div>
         </div>
         {bidList.map((obj, i) => (
-          <li key={i} className={styles.askBidItem}>
+          <li
+            key={i}
+            className={`${styles.askBidItem} ${styles.askBidInnerGreen}`}
+          >
             <span>{obj[0]}</span>
             <br />
             <span>{obj[1]}</span>
